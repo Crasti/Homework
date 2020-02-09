8f9ff8801f75f8699e1c99624df402d045bd5cbe
Roman Andreev <c_crasti@rambler.ru>
2020-02-09 14:12:20 +0300

    Дополнено Задание 9 БД

--- DB/DB9.sql
+++ DB/DB9.sql
@@ -4,8 +4,8 @@
 -- Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
 
 START TRANSACTION; 
-INSERT INTO sample.users SELECT * FROM shop.users WHERE id = 1;
-DELETE FROM shop.users WHERE id = 1 LIMIT 1;
+  INSERT INTO sample.users SELECT * FROM shop.users WHERE id = 1;
+  DELETE FROM shop.users WHERE id = 1 LIMIT 1;
 COMMIT;
 -- Если я правильно понял, и имеется ввиду переместить всю строку с id = 1 в такую же таблицу
 
@@ -18,9 +18,87 @@ SELECT p.name, c.name AS category
 FROM products p
 JOIN catalogs c
 ON p.catalog_id = c.id
-WHERE p.id = 3;
+-- WHERE p.id = 3;
 
 SELECT * FROM product;
+
+-- 3.Пусть имеется таблица с календарным полем created_at.
+-- В ней размещены разряженые календарные записи за август 2018 года '2018-08-01', '2018-08-04', 
+-- '2018-08-16' и 2018-08-17. Составьте запрос, который выводит полный список дат за август, 
+-- выставляя в соседнем поле значение 1, если дата присутствует в исходном таблице и 0, если она отсутствует.
+
+CREATE TABLE IF NOT EXISTS posts (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  created_at DATE NOT NULL
+);
+
+INSERT INTO posts VALUES
+(NULL, 'первая запись', '2018-08-01'),
+(NULL, 'вторая запись', '2018-08-04'),
+(NULL, 'третья запись', '2018-08-16'),
+(NULL, 'четвертая запись', '2018-08-17');
+
+CREATE TEMPORARY TABLE last_days (
+  day INT
+);
+
+INSERT INTO last_days VALUES
+(0), (1), (2), (3), (4), (5), (6), (7), (8), (9), (10),
+(11), (12), (13), (14), (15), (16), (17), (18), (19), (20),
+(21), (22), (23), (24), (25), (26), (27), (28), (29), (30);
+
+SELECT
+  DATE(DATE('2018-08-31') - INTERVAL l.day DAY) AS day,
+  NOT ISNULL(p.name) AS order_exist
+FROM
+  last_days AS l
+LEFT JOIN
+  posts AS p
+ON
+  DATE(DATE('2018-08-31') - INTERVAL l.day DAY) = p.created_at
+ORDER BY
+  day;
+
+-- 4.Пусть имеется любая таблица с календарным полем created_at. 
+-- Создайте запрос, который удаляет устаревшие записи из таблицы, оставляя
+-- только 5 самых свежих записей.
+
+DROP TABLE IF EXISTS posts;
+CREATE TABLE IF NOT EXISTS posts (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  created_at DATE NOT NULL
+);
+
+INSERT INTO posts VALUES
+(NULL, 'первая запись', '2018-11-01'),
+(NULL, 'вторая запись', '2018-11-02'),
+(NULL, 'третья запись', '2018-11-03'),
+(NULL, 'четвертая запись', '2018-11-04'),
+(NULL, 'пятая запись', '2018-11-05'),
+(NULL, 'шестая запись', '2018-11-06'),
+(NULL, 'седьмая запись', '2018-11-07'),
+(NULL, 'восьмая запись', '2018-11-08'),
+(NULL, 'девятая запись', '2018-11-09'),
+(NULL, 'десятая запись', '2018-11-10');
+
+DELETE
+  posts
+FROM
+  posts
+JOIN
+ (SELECT
+    created_at
+  FROM
+    posts
+  ORDER BY
+    created_at DESC
+  LIMIT 5, 1) AS delpst
+ON
+  posts.created_at <= delpst.created_at;
+
+SELECT * FROM posts;
 
 
 -- “Хранимые процедуры и функции, триггеры"
@@ -31,40 +109,90 @@ SELECT * FROM product;
 -- с 00:00 до 6:00 — "Доброй ночи".
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS hello//
-CREATE PROCEDURE hello()
+DROP FUNCTION IF EXISTS hello//
+CREATE FUNCTION hello()
+  RETURNS TINYTEXT NO SQL
   BEGIN
-	  SET @times = (SELECT HOUR(NOW()));
-	  IF (@times >= '6') AND (@times < '12') THEN
-		SELECT 'Доброе утро' AS Приветствие;
-	  ELSEIF (@times >= '12') AND (@times < '18') THEN
-		SELECT 'Добрый день' AS Приветствие;
-	  ELSEIF (@times >= '18') AND (@times < '24') THEN
-		SELECT 'Добрый вечер' AS Приветствие;
-	  ELSE (@times >= '0') AND (@times < '6') THEN
-		SELECT 'Доброй ночи' AS Приветствие;
+	  DECLARE hour INT;
+	  SET hour = HOUR(NOW());
+	  IF (hour >= '6') AND (hour < '12') THEN
+		RETURN 'Доброе утро';
+	  ELSEIF (hour >= '12') AND (hour < '18') THEN
+		RETURN 'Добрый день';
+	  ELSEIF (hour >= '18') AND (hour < '24') THEN
+		RETURN 'Добрый вечер';
+	  ELSE 
+	    RETURN 'Доброй ночи';
       END IF;
   END//
-
-/* SHOW PROCEDURE STATUS;
-CALL hello();
-SELECT HOUR(NOW());
-SELECT 'Доброе утро' AS Приветствие;
-SELECT @times */
+  
+DELIMITER ;
+  
+SELECT NOW(), hello ();
 
 
   
  /* В таблице products есть два текстовых поля: name с названием товара и description с его описанием. 
  Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное 
  значение NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля 
- были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию. /*
+ были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию. */
  
  
  
+DELIMITER //
+DROP TRIGGER IF EXISTS control_name_description_insert//
+CREATE TRIGGER control_name_description_insert BEFORE INSERT ON products
+FOR EACH ROW BEGIN
+  IF NEW.name IS NULL AND NEW.description IS NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Оба поля не могут быть NULL';
+  END IF;
+END//
+DELIMITER ;
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  (NULL, NULL, 5000.00, 2);
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('ASUS PRIME Z370-P', 'HDMI, SATA3, PCI Express 3.0,, USB 3.1', 7600.00, 2);
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  (NULL, 'HDMI, SATA3, PCI Express 3.0,, USB 3.1', 7600.00, 2);
+
+DELIMITER //
+DROP TRIGGER IF EXISTS control_name_description_update//
+CREATE TRIGGER control_name_description_update BEFORE UPDATE ON products
+FOR EACH ROW BEGIN
+  IF NEW.name IS NULL AND NEW.description IS NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Оба поля не могут быть NULL';
+  END IF;
+END//
+DELIMITER ;
  
  
- 
- 
- 
+/* 3.Напишите хранимую функцию для вычисления произвольного числа Фибоначчи. 
+Числами Фибоначчи называется последовательность в которой число равно сумме двух предыдущих чисел.
+Вызов функции FIBONACCI(10) должен возвращать число 55. 
+
+DELIMITER //
+
+CREATE FUNCTION FIBONACCI(num INT)
+RETURNS INT DETERMINISTIC
+BEGIN
+  DECLARE fs DOUBLE;
+  SET fs = SQRT(5);
+
+  RETURN (POW((1 + fs) / 2.0, num) + POW((1 - fs) / 2.0, num)) / fs;
+END//
+
+SELECT FIBONACCI(10)// 
+*/
 
 
